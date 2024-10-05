58b7cf3ae21b52e6a7fa22a1e29b82d172033e12
Philippe DELENTE <philippe.delente@gmail.com>
2024-10-05 13:53:33 +0200

    fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,7 +9,12 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'yoda_style' => true,
-        'array_syntax' => ['syntax' => 'short']
+        'array_syntax' => ['syntax' => 'short'],
+        'single_quote' => true,
+        'no_trailing_whitespace' => true,
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'no_unused_imports' => true,
     ])
 
     ->setFinder($finder)
