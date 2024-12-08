5b82510fcd2b92182f01cb9634e94cb43ebbb5a2
Philippe Delente <philippe.delente@gmail.com>
2024-12-08 16:45:53 +0100

    update symfony

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
